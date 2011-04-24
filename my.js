8c996e950dd1ebc77913c41007c5555c2c74e973
Kádár Tamás <ktamas@ktamas.com>
2011-04-24 15:10:37 +0200

    come on

---
+++ my.js
@@ -0,0 +1,35 @@
+var getwords = function() {
+  var words = [];
+  $('input').each(function(c, item) {
+    var value = $(item).val();
+    if (value != '') {
+      words.push(value);
+    }
+  });
+  console.log(words);
+  return words;
+}
+
+$(document).ready(function(){
+  $('input').bind('blur', function(event) {
+    if (getwords().length < 3) {
+      return;
+    }
+
+    if (getwords().length == 16) {
+      return; // don't re-request
+    }
+    $.ajax({ 
+      type: 'POST',
+      url: '/gimme',
+      data: { words: getwords() },
+      dataType: 'json',
+      success: function(body) {
+        $('input').each(function(c, item) {
+          $(item).val(body[c]);
+        });
+      },
+    error: function() { alert("oops"); }
+    });
+  });
+});
